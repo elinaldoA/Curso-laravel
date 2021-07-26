a5c1d040c18a777b5fd698aef9609327897cdaf9
elinaldoA <83353178+elinaldoA@users.noreply.github.com>
2021-07-25 21:57:47 -0300

    Trabalhando com materialize

--- routes/web.php
+++ routes/web.php
@@ -24,15 +24,15 @@ Route::post('/contato', ['uses'=>'App\Http\Controllers\ContatoController@criar']
 Route::put('/contato', ['uses'=>'App\Http\Controllers\ContatoController@editar']);
 
 //Definindo apelido para as rotas
-Route::get('/admin/cursos', ['as'=>'admin.cursos','uses'=>'App\Http\Admin\CursosController@index']);
+Route::get('/admin/cursos', ['as'=>'admin.cursos','uses'=>'App\Http\Controllers\Admin\CursoController@index']);
 //Adcionar novos
-Route::get('/admin/cursos/adicionar', ['as'=>'admin.cursos.adicionar','uses'=>'App\Http\Admin\CursosController@adicionar']);
+Route::get('/admin/cursos/adicionar', ['as'=>'admin.cursos.adicionar','uses'=>'App\Http\Controllers\Admin\CursoController@adicionar']);
 //Salvar 
-Route::post('/admin/cursos/salvar', ['as'=>'admin.cursos.salvar','uses'=>'App\Http\Admin\CursosController@salvar']);
+Route::post('/admin/cursos/salvar', ['as'=>'admin.cursos.salvar','uses'=>'App\Http\Controllers\Admin\CursoController@salvar']);
 //Editar
-Route::get('/admin/cursos/editar/{id}', ['as'=>'admin.cursos.editar','uses'=>'App\Http\Admin\CursosController@editar']);
+Route::get('/admin/cursos/editar/{id}', ['as'=>'admin.cursos.editar','uses'=>'App\Http\Controllers\Admin\CursoController@editar']);
 //Atualizar
-Route::get('/admin/cursos/atualizar/{id}', ['as'=>'admin.cursos.atualizar','uses'=>'App\Http\Admin\CursosController@atualizar']);
+Route::get('/admin/cursos/atualizar/{id}', ['as'=>'admin.cursos.atualizar','uses'=>'App\Http\Controllers\Admin\CursoController@atualizar']);
 //Deletar
-Route::get('/admin/cursos/deletar/{id}', ['as'=>'admin.cursos.deletar','uses'=>'App\Http\Admin\CursosController@deletar']);
+Route::get('/admin/cursos/deletar/{id}', ['as'=>'admin.cursos.deletar','uses'=>'App\Http\Admin\Controllers\CursoController@deletar']);
 
